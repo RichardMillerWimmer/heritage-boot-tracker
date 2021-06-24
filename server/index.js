492cd40dc1589cf7373be1f92e9c19c9bb2fa846
Richard Miller <richardmmill@gmail.com>
2021-06-24 15:17:02 -0400

    server/index.js

---
+++ server/index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const authMiddleware = require('./middleware/authMiddleware');
+const auth = require('./controllers/authController');
+
+
+const app = express();
+
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
+
+app.use(express.json());
+// app.use(express.static(`${__dirname}/../build`));
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  })
+);
+
+//Auth endpoints
+app.post('/api/auth/register', auth.register);
+app.post('/api/auth/login', auth.login);
+app.get('/api/auth/user', authMiddleware.authorize, auth.getUser);
+app.delete('/api/auth/logout', auth.logout);
+
+//Boot endpoints
+app.get('/api/boot/boot');
+app.get('/api/boot/boot/:id');
+app.post('/api/boot/add');
+app.post('/api/boot/wear');
+app.post('/api/boot/cc');
+app.put('/api/boot/boot');
+app.delete('/api/boot/boot');
+
+//User endpoints
+app.put('/api/user/:editType');
+app.delete('/api/user/delete');
+
+//Friend endpoints
+app.get('/api/friend/people');
+app.get('api/friend/people/:id');
+app.post('/api/friend/people');
+app.delete('/api/friend/people');
+app.get('/api/friend/list');
+app.get('api/friend/list/:id');
+
+// Password Reset
+// app.put('/api/pwdReset/reqest', passwordReset.resetPwdEmail);
+// app.put('/api/pwdReset/submit/:token', passwordReset.processReset);
+
+
+massive({
+  connectionString: CONNECTION_STRING,
+  // @ts-ignore
+  ssl: { rejectUnauthorized: false }
+})
+  .then((dbInstance) => {
+    app.set('db', dbInstance);
+    app.listen(SERVER_PORT, () => console.log(`DB and Server Connected to Port ${SERVER_PORT}`));
+  })
+  .catch((err) => console.log(err));
