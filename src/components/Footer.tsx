bc8253cfa4bbb6fe154a5f9190907077abb76ddb
Richard Miller <richardmmill@gmail.com>
2021-06-24 14:49:21 -0400

    more initial setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+
+    return (
+        <div>
+            <h3>Footer Component</h3>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+
+    return (
+        <div>
+            <h3>Header Component</h3>
+        </div>
+    )
+}
+
+export default Header;
