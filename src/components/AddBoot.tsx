7fd4c89891b666a5a856934e30cdd8d643dbcd64
Richard Miller <richardmmill@gmail.com>
2021-07-15 13:49:15 -0400

    work on addBoot sass

--- src/components/AddBoot.tsx
+++ src/components/AddBoot.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
+import Button from './Button';
+
 const AddBoot: React.FC = () => {
     const [name, setName] = useState<string>('');
     const [img, setImg] = useState<string>('');
@@ -26,24 +28,24 @@ const AddBoot: React.FC = () => {
     };
 
     return (
-        <div>
-            <h3>Add Boot</h3>
+        <div className='addBoot'>
             <form
             id="addBootForm"
             onSubmit={(e: React.SyntheticEvent) => {
                 e.preventDefault();
                 addBoot();
             }}>
-                <label htmlFor="addName">name:</label>
+                <h3>Add Boot</h3>
+                <label htmlFor="addName"></label>
                 <input 
                 id="addName"
                 type="text"
                 value={name}
-                placeholder="make"
+                placeholder="boot"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setName(e.target.value)}
                  />
                  <br></br>
-                <label htmlFor="addWears">wears:</label>
+                <label htmlFor="addWears"></label>
                 <input 
                 id="addWears"
                 type="number"
@@ -54,27 +56,29 @@ const AddBoot: React.FC = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setWears(e.target.value)}
                  />
                  <br></br>
-                <label htmlFor="addCcs">clean and conditions:</label>
+                <label htmlFor="addCcs"></label>
                 <input 
                 id="addCcs"
                 type="number"
                 min="0"
                 step="1"
                 value={ccs}
-                placeholder="Clean and Conditions"
+                placeholder="clean and conditions"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setCcs(e.target.value)}
                  />
                  <br></br>
-                <label htmlFor="addNote">notes:</label>
+                <label htmlFor="addNote"></label>
                 <textarea 
                 id="addNote"
                 form="addBootForm"
                 value={note}
                 placeholder="notes"
+                rows={10}
+                cols={30}
                 onChange={(e: React.ChangeEvent<HTMLTextAreaElement>): void => setNote(e.target.value)}
                  />
                  <br></br>
-                <label htmlFor="addImage">image:</label>
+                <label htmlFor="addImage"></label>
                 <input 
                 id="addImage"
                 type="text"
@@ -83,7 +87,7 @@ const AddBoot: React.FC = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setImg(e.target.value)}
                  />
                  <br />
-                 <button>add boot</button>
+                 <Button>add boot</Button>
             </form>
         </div>
     )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,10 @@ import styled from "@emotion/styled";
 
 const Button = styled.button`
     font-family: 'Montserrat', sans-serif;
+    padding: 7px 25px;
     color: white;
     background: rgb(193, 17, 41);
-    border: 2px solid rgb(193, 17, 41);
+    border: 3px solid rgb(193, 17, 41);
     cursor: pointer;
     &:hover {
         color: black;
